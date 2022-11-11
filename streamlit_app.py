112202a05c3dc80febe0602fc4351bf56ef3c3e3
shrisailaeshraja <117992636+shrisailaeshraja@users.noreply.github.com>
2022-11-11 18:36:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
+import streamlit
 
+streamlit.title('my parents healthy dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('kale.spinach & rocket smoothie')
+streamlit.text('Hard boiled free range egg')

c0526b44fc95e0e3a6bbddd546bd18311deca111
shrisailaeshraja <117992636+shrisailaeshraja@users.noreply.github.com>
2022-11-12 16:02:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,8 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("Hello from Snowflake:")
 streamlit.dataframe(my_data_row)
+
+my_add_fruit = streamlit.text.input('what fruit do you want to add?','apple')
+streamlit.write('the user entered',my_add_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + my_add_fruit)
+
